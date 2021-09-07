5a713215e91dd69cb85fac57c63b3cf970b38171
denn409e <denn409e@edu.easj.dk>
2021-09-07 10:26:29 +0200

    Updating wrong solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// Defining Variables
     String intro = "Today's date is ";
-    String day = "Tuesday, ";
-    String date = "7th, ";
+    String day = "Tuesday ";
+    String date = "7th ";
     String month = "September ";
     int year = 2021;
 
-    //print of the defined outputs to show today's date
-    System.out.print(intro);
-    System.out.print(day);
-    System.out.print(month);
-    System.out.print(date);
+    //American standard
+    System.out.print((intro) + (day)  + (month) + (date));
+    System.out.println(year);
+
+    //European standard
+    System.out.print((intro) + (day) + (date) + (month));
     System.out.println(year);
     }
 }
