f9f0fc6a5c14e116422c338fa154b52a1055b3f8
denn409e <denn409e@edu.easj.dk>
2021-09-07 09:54:35 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Defining Variables
+    String intro = "Today's date is ";
+    String day = "Tuesday, ";
+    String date = "7th, ";
+    String month = "September ";
+    int year = 2021;
+
+    //print of the defined outputs to show today's date
+    System.out.print(intro);
+    System.out.print(day);
+    System.out.print(month);
+    System.out.print(date);
+    System.out.println(year);
+    }
+}
